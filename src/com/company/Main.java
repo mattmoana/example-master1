bbdba9192d02c844011ddc3845d5fe8050adbae0
Matt <matt.morozov@cyberland.io>
2022-11-20 14:59:55 +0200

    переименовал класс + добавил подсчет ответов

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,5 +15,6 @@ public class Main {
         } else {
             System.out.println("Error!");
         }
+
     }
 }
--- src/homeWork1/Quiz.java
+++ src/homeWork1/Quiz.java
@@ -2,39 +2,49 @@ package homeWork1;
 
 import java.util.Scanner;
 
-public class Main {
+public class Quiz {
 
     public static void main(String[] args) {
+        int correctAnswers = 0;
+        int incorrectAnswers = 0;
         System.out.println("Выберите правильный вариант ответа.");
         String question1 = "\nКак хочет полететь на луну? \n1) Jeff Bezos\n2) Elon Musk\n3) Joe Biden ";
         System.out.println(question1);
         Scanner in1 = new Scanner(System.in);
         String answer1 = in1.nextLine();
-        Integer intAnswer1 = Integer.parseInt(answer1);
+        int intAnswer1 = Integer.parseInt(answer1);
         if (intAnswer1 == 2){
             System.out.println("Ура! Это правильный вариант!");
+            correctAnswers += 1;
         } else {
             System.out.println("Бууу... неправильный вариант!");
+            incorrectAnswers += 1;
         }
         String question2 = "\nКак называется язык програмирования похожий на змею?\n1) Python\n2) Cobra\n3) Anaconda";
         System.out.println(question2);
         Scanner in2 = new Scanner(System.in);
         String answer2 = in2.nextLine();
-        Integer intAnswer2 = Integer.parseInt(answer2);
+        int intAnswer2 = Integer.parseInt(answer2);
         if (intAnswer2 == 1){
             System.out.println("Ура! Это правильный вариант!");
+            correctAnswers += 1;
         } else {
             System.out.println("Бууу... неправильный вариант!");
+            incorrectAnswers += 1;
         }
         String question3 = "\nКакая самая большая пирамида?\n1) Pyramid of Khufu\n2) Pyramid of Khafre\n3) Great Pyramid of Cholula";
         System.out.println(question3);
         Scanner in3 = new Scanner(System.in);
         String answer3 = in3.nextLine();
-        Integer intAnswer3 = Integer.parseInt(answer3);
+        int intAnswer3 = Integer.parseInt(answer3);
         if (intAnswer3 == 1){
             System.out.println("Ура! Это правильный вариант!");
+            correctAnswers += 1;
         } else {
             System.out.println("Бууу... неправильный вариант!");
+            incorrectAnswers += 1;
         }
+        System.out.println("\n\nПравильных ответов: " + correctAnswers + "\nНеправильных ответов: " + incorrectAnswers);
+
     }
 }
